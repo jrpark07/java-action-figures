e34f8224419f69c5196f80977e0ea0afa9340386
JR <jrpark07@gmail.com>
2021-07-02 18:10:33 -0700

    Sixth project

---
+++ src/main/java/com/launchacademy/javathymeleafexercise/services/ProductService.java
@@ -0,0 +1,11 @@
+package com.launchacademy.javathymeleafexercise.services;
+
+import com.launchacademy.javathymeleafexercise.models.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductService {
+  List<Product> getProducts();
+  void addToProduct(Product product);
+
+}
